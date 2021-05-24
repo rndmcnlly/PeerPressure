9234e9e5b9b788b3cab740fe3a3b1a9e9987ef8c
Adam Smith <adam@adamsmith.as>
2021-05-24 12:41:13 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,60 @@
+const HOST_ID = 'PeerPressure';
+
+class Playground extends Phaser.Scene {
+    constructor() {
+        super("playground");
+    }
+
+    create() {
+        this.input.keyboard.on('keydown-A', () => this.host());
+        this.input.keyboard.on('keydown-B', () => this.join());
+    }
+
+    host() {
+        let peer = new Peer(HOST_ID);
+        peer.on('open', function(id) {
+            console.log('open with', id);
+        });
+        peer.on('connection', (conn) => {
+            console.log('connection with ', conn);
+            let player = this.makePlayer(conn.id);
+            conn.on('data', (data) => {
+                console.log('data!', data, conn);
+                let {x,y} = data;
+                player.x = x;
+                player.y = y;
+            });
+        });
+    }
+
+    join() {
+        let peer = new Peer();
+        
+        peer.on('open', (id) => {
+            console.log('open with', id);
+            let player = this.makePlayer(id);
+            let conn = peer.connect(HOST_ID);
+            conn.on('open', () => {
+                console.log('connected to host');
+                this.input.on('pointermove', (pointer) => {
+                    console.log(pointer);
+                    player.x = pointer.x;
+                    player.y = pointer.y;
+                    conn.send({x: player.x, y: player.y});
+                });
+            });
+        });
+        
+    }
+
+    makePlayer(id) {
+        let player = this.add.graphics();
+        player.fillStyle(0xFFFFFF, 1.0);
+        player.fillRect(0,0,10,10);
+        return player;
+    }
+}
+
+const game = new Phaser.Game({
+    scene: [Playground]
+});
